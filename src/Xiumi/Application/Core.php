74170a9a98a0d2545fe6dbf2abbf936c20a9ccad
rmrhz <rupaheizu@hakz.co>
2015-10-27 08:16:16 +0800

    Base bindings for to bootstrap 'Illuminate\Container\Container'

--- src/Xiumi/Application/Core.php
+++ src/Xiumi/Application/Core.php
@@ -2,12 +2,32 @@
 
 namespace Xiumi\Application;
 
+use Illuminate\Container\Container;
+
 class Core
 {
-
+	/**
+	 * Boots Xiumi
+	 *
+	 * @return void
+	 */
 	public function __construct()
 	{
+		$this->registerBaseBindings();
+	}
 
+	/**
+	 * Register the basic bindings into the container.
+	 *
+	 * @return void
+	 */
+	public function registerBaseBindings()
+	{
+		$this->app = new Container;
+
+		$this->instance('app', $this);
+
+		$this->instance('Illuminate\Container\Container', $this);
 	}
 	
 }
