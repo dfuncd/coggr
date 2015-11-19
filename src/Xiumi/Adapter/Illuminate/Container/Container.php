f765c660de31ab074f83aeecc771db70114cdb04
rmrhz <rmrhz@tfwno.gf>
2015-11-19 18:14:47 +0800

    Added Illuminate Container adapter

---
+++ src/Xiumi/Adapter/Illuminate/Container/Container.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Xiumi\Adapter\Illuminate\Container;
+
+use Illuminate\Container\Container as IlluminateContainer;
+use Xiumi\Contract\Container\Container as ContainerInterface;
+
+class Container extends IlluminateContainer implements ContainerInterface
+{
+	
+}
