2e248f7374b658f6a4cff2ee1d38ae160f3787e5
rmrhz <rmrhz@tfwno.gf>
2015-11-21 23:44:22 +0800

    Initial concept for container adapter

--- src/Xiumi/Adapter/Illuminate/Container/Container.php
+++ src/Xiumi/Adapter/Illuminate/Container/Container.php
@@ -8,4 +8,26 @@ use Xiumi\Contract\Container\Container as ContainerInterface;
 class Container extends IlluminateContainer implements ContainerInterface
 {
 	
+	/**
+	 * Gets an instance from the container
+	 *
+	 * @param string $id
+	 * @return obj
+	 */
+	public function get($id)
+	{
+		
+	}
+
+	/**
+	 * Sets an instance to the container
+	 *
+	 * @param string $id
+	 * @return void
+	 */
+	public function set($id)
+	{
+
+	}
+
 }
