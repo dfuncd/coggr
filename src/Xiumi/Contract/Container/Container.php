457b01b4d79b9ee8793e713262f24bed48e04f7d
rmrhz <rmrhz@tfwno.gf>
2015-11-25 23:52:27 +0800

    Updated set function requirements

--- src/Xiumi/Contract/Container/Container.php
+++ src/Xiumi/Contract/Container/Container.php
@@ -4,5 +4,14 @@ namespace Xiumi\Contract\Contianer;
 
 interface Container extends \Interop\Container\ContainerInterface
 {
-	
+
+	/**
+	 * Register an existing instance as shared in the container.
+	 *
+	 * @param  string  $abstract
+	 * @param  mixed   $instance
+	 * @return void
+	 */
+	public function set($abstract, $instance);
+
 }
