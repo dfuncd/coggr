ac0a221b515635889caa03e728265e1f465fd0b6
rmrhz <rmrhz@tfwno.gf>
2015-11-29 22:36:12 +0800

    Added essential function for IoC container

--- src/Xiumi/Application/Core.php
+++ src/Xiumi/Application/Core.php
@@ -42,5 +42,87 @@ class Core
 
 		$this->instance('app', $this);
 	}
+
+	/**
+	 * Register a service provider with the application.
+	 *
+	 * @param  TakeiProviderProvider|string  $provider
+	 * @param  array  $options
+	 * @param  bool   $force
+	 * @return TakeiProviderProvider
+	 */
+	public function register($provider, $options = [], $force = false)
+	{
+		if ( $registered = $this->getProvider($provider) && ! $force ) {
+			return $registered;
+		}
+
+		// If the given "provider" is a string, we will resolve it, passing in the
+		// application instance automatically for the developer. This is simply
+		// a more convenient way of specifying your service provider classes.
+		if (is_string($provider)) {
+			$provider = $this->resolveProviderClass($provider);
+		}
+
+		$provider->register();
+
+		// Once we have registered the service we will iterate through the options
+		// and set each of them on the application so they will be available on
+		// the actual loading of the service objects and for developer usage.
+		foreach ($options as $key => $value) {
+			$this[$key] = $value;
+		}
+
+		$this->markAsRegistered($provider);
+
+		// If the application has already booted, we will call this boot method on
+		// the provider class so it has an opportunity to do its boot logic and
+		// will be ready for any usage by the developer's application logics.
+		if ($this->booted) {
+			$this->bootProvider($provider);
+		}
+	}
+
+	/**
+	 * Get the registered service provider instance if it exists.
+	 *
+	 * @param  TakeiProviderProvider|string  $provider
+	 * @return TakeiProviderProvider|null
+	 */
+	public function getProvider($provider)
+	{
+		$name = is_string($provider) ? $provider : get_class($provider);
+
+		foreach($this->serviceProviders as $key => $value) {
+			call_user_func_array(function ($key, $value) use ($name) {
+				return $value instanceof $name;
+			}, []);
+		}
+	}
+
+	/**
+	 * Resolve a service provider instance from the class name.
+	 *
+	 * @param  string  $provider
+	 * @return TakeiProviderProvider
+	 */
+	public function resolveProviderClass($provider)
+	{
+		return new $provider($this);
+	}
+
+	/**
+	 * Mark the given provider as registered.
+	 *
+	 * @param TakeiProviderProvider
+	 * @return void
+	 */
+	protected function markAsRegistered($provider)
+	{
+		$class = get_class($provider);
+
+		$this->serviceProviders[] = $provider;
+		$this->loadedProviders[$class] = true;
+	}
 	
 }
