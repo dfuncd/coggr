6890504a3408480000c493d78d06a0ad4bbc4f52
rmrhz <rmrhz@tfwno.gf>
2015-12-03 23:48:32 +0800

    Reconceptualize

--- src/Coggr/Adapter/Illuminate/Container/Container.php
+++ src/Coggr/Adapter/Illuminate/Container/Container.php
@@ -8,27 +8,4 @@ use Coggr\Contract\Container\Container as ContainerInterface;
 class Container extends IlluminateContainer implements ContainerInterface
 {
 
-	/**
-	 * Gets an instance from the container
-	 *
-	 * @param string $id
-	 * @return obj
-	 */
-	public function get($id)
-	{
-		return $this->getConcrete($id);
-	}
-
-	/**
-	 * Register an existing instance as shared in the container.
-	 *
-	 * @param  string  $abstract
-	 * @param  mixed   $instance
-	 * @return void
-	 */
-	public function set($id)
-	{
-		return $this->instance($abstract, $instance);
-	}
-
 }
--- src/Coggr/Contract/Container/Container.php
+++ src/Coggr/Contract/Container/Container.php
@@ -5,13 +5,4 @@ namespace Coggr\Contract\Contianer;
 interface Container
 {
 
-	/**
-	 * Register an existing instance as shared in the container.
-	 *
-	 * @param  string  $abstract
-	 * @param  mixed   $instance
-	 * @return void
-	 */
-	public function set($abstract, $instance);
-
 }
