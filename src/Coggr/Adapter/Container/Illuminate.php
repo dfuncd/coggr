a5196cb63e07c84a8434e62a087aaa114691ada0
rmrhz <rmrhz@tfwno.gf>
2015-12-06 20:58:20 +0800

    Renamed namespace and class name

--- src/Coggr/Adapter/Container/Illuminate.php
+++ src/Coggr/Adapter/Container/Illuminate.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Coggr\Adapter\Illuminate\Container;
+namespace Coggr\Adapter\Container;
 
 use Illuminate\Container\Container as IlluminateContainer;
 use Coggr\Contract\Container\Container as ContainerInterface;
 
-class Container extends IlluminateContainer implements ContainerInterface
+class Illuminate extends IlluminateContainer implements ContainerInterface
 {
 	
 }
