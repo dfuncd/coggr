2ffe5f5456e2142204849672e7ec65568c0d4c78
rmrhz <rmrhz@tfwno.gf>
2016-05-23 14:38:00 +0900

    Added provider abstract class

---
+++ src/Coggr/Application/Provider.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Coggr\Application;
+
+use Coggr\Application\System;
+
+abstract class Provider
+{
+
+	/**
+	 * System application container
+	 *
+	 * @var \Coggr\Application\System
+	 */
+	protected $system;
+
+	/**
+	 * Classes that are provided by the service
+	 *
+	 * @var array
+	 */
+	protected $provides = [];
+
+	/**
+	 * Class constructor
+	 *
+	 * @param \Coggr\Application\System $system
+	 */
+	public function __construct(System $system)
+	{
+		$this->system = $system;
+	}
+
+	/**
+	 * @inheritdocs
+	 */
+	public function get($name)
+	{
+		return $this->system->get($name);
+	}
+
+	/**
+	 * @inheritdocs
+	 *
+	 * @return $this
+	 */
+	public function set($name, $object)
+	{
+		$this->system->set($name, $object);
+
+		return $this;
+	}
+
+	abstract public function register();
+
+	/**
+	 * Return list an array of classes that are provided
+	 *
+	 * @return array
+	 */
+	public function getProvides()
+	{
+		return $this->provides;
+	}
+
+}
